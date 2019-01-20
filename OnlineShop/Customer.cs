8133957a0a39c0bb78d433e71114770210376cb0
Андрей М <librandron@gmail.com>
2019-01-20 16:35:46 +0300

    add classes

---
+++ OnlineShop/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop
+{
+    public class Customer
+    {
+        public int id;
+        public string firstName;
+        public string secondName;
+
+    }
+}
